4dc8b78e42b69ae9ae451c869093a76ace50aec2
pradeep761 <pradeepyanala25@gmail.com>
2024-10-13 23:36:27 -0500

    Create User.js
    This JavaScript code defines a User model using Mongoose, an Object Data Modeling (ODM) library for MongoDB and Node.js. Additionally, it incorporates bcryptjs for securely handling user passwords.

---
+++ MongoDB Configuration (config/models/User.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+
+const userSchema = mongoose.Schema({
+  email: {
+    type: String,
+    required: [true, 'Please add an email'],
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: [true, 'Please add a password'],
+  },
+}, {
+  timestamps: true,
+});
+
+userSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) {
+    next();
+  }
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+});
+
+module.exports = mongoose.model('User', userSchema);
